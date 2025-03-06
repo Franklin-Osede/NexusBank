c5f6059a6d7d42be3a19499e6f4cfbc5158fab4f
Franklin <71532004+fran1014@users.noreply.github.com>
2025-03-06 14:50:45 +0100

    fix: resolve springboot depencies issue

---
+++ nexusbank/src/main/java/com/nexusbank/NexusBankApplication.java
@@ -0,0 +1,14 @@
+package com.nexusbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class NexusBankApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NexusBankApplication.class, args);
+  }
+}
